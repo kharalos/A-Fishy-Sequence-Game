21e68b949afbac8234bde5673d0d6cf91afd83ea
Kharalos <49806742+kharalos@users.noreply.github.com>
2021-10-15 20:37:10 +0300

    fish in aquarium

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -6,14 +6,18 @@ public class Fish : MonoBehaviour
 {
     public GameObject target;
     Rigidbody body;
+    Animator anim;
+    public float distanceValue;
     private void Start()
     {
         body = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
     }
-    // Update is called once per frame
     void FixedUpdate()
     {
         body.MovePosition((body.position + (target.transform.position - body.position) * 0.2f * Time.deltaTime));
         transform.LookAt(target.transform);
+        distanceValue = (target.transform.position - body.position).magnitude;
+        anim.SetFloat("Swim", distanceValue);
     }
 }
